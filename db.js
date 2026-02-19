1f79eaa8f3d0075967f74b6116071bad0c11fce6
darainShah <darainshahedi1729@gmail.com>
2026-02-19 13:57:01 +0530

    shifted mongodb server to online

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // MongoDB connection URL
-const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
+//const mongoURL = 'mongodb://127.0.0.1:27017/hotels';
+//const mongoURL = 'mongodb+srv://darain:Darain@1729@cluster0.9o91lfs.mongodb.net/'
+const mongo_url = process.env.mongodb_url;
+//const mongo_url_local = process.env.mongodb_url_local;
 
 // Connect to MongoDB
 mongoose
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 require('./db'); // just require, no variable
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
